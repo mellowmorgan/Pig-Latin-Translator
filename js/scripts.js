05e70d312bc5885f3ec0837872bfff2f028842f7
mellowmorgan <morgan.waites@gmail.com>
2021-10-27 14:59:49 -0700

    Add function stripPunctuation
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,39 @@
-function vowelChecker(letter){
-  const array =  ['a','e','i','o','u'];
-  for(let i=0;i<5;i++){
-    if(letter===array[i]){
-      return true;
-    }
-  }
-  return false;
+// function vowelChecker(letter){
+//   const array =  ['a','e','i','o','u'];
+//   for(let i=0;i<5;i++){
+//     if(letter===array[i]){
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+function checkIfStringHasSpecialChar(string)
+{ 
+  string = string.replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/g, "");
+  return string;
 }
 
+
+
+
 function translator (word){
+  //if its got nonalphabetical characters return 0
+  // if(word.match(/\d+/g)){return 0;}
+  if(/[^a-z]/.test(word)){return 0;}
+
   const vowelSuffix = "way";
   let consonantSuffix = "ay";
   let newWord;
   let letter = "";
-  if(vowelChecker(word.charAt(0))){
+
+  if(/[aieou]/.test(word.charAt(0))){
     newWord = word.concat(vowelSuffix);
     return newWord;
   } else{
       for(let i = 0; i < word.length; i++) {
       letter += word.charAt(i);
-      if(vowelChecker(word.charAt(i+1))&&(!(word.charAt(0)==='q'))){
+      if((/[aeiou]/.test(word.charAt(i+1)))&&(!(word.charAt(0)==='q'))){
         consonantSuffix=letter+consonantSuffix;
         word=word.slice(i+1,word.length);
         newWord=word.concat(consonantSuffix);
